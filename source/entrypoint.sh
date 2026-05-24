e17141b868ccfeb1a1df26f93f2cef8e5707214d
thelastblt <166474022+thelastblt@users.noreply.github.com>
2026-05-24 14:58:32 -0700

    Change network_mode to personal_cloud in config.ini
    Update network_mode in config.ini to personal_cloud for remote access.

--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -81,3 +81,16 @@ else
             --disable-auto-launch \
             "$@"
 fi
+
+# When the server is launched with --listen and is bound to a network range other than the local 127. range, 
+# allowing remote IP access, the security levels are altered. To allow for custom nodes install, Need to update 
+# the setting network mode to 'personal_cloud' to allow you to have the same access as 'normal' security level 
+# does on a local install.
+sed -i 's/network_mode = .*/network_mode = personal_cloud/' user/__manager/config.ini
+
+if [ $? -eq 0 ]; then
+    echo "Successfully updated network_mode to personal_cloud in config.ini"
+else
+    echo "Failed to update network_mode in config.ini. Custom nodes installation, updates, and model installations may not work properly."
+    exit 1
+fi
